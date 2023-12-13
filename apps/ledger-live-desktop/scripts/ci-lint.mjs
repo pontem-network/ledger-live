dd57771f176a838f85b5a48b8f3e2ca4823d1e6b
jdabbech-ledger <jalil.dabbech@ledger.com>
2023-12-13 17:02:22 +0100

    chore: use of zx instead of bash

---
+++ apps/ledger-live-desktop/scripts/ci-lint.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env zx
+import { basename } from 'path'
+import stylish from "../../../node_modules/eslint/lib/cli-engine/formatters/stylish.js"
+
+const usage = () => {
+  console.log(`Usage: ${basename(__filename)} [-h] [-p <port>] `);
+  process.exit(1)
+}
+
+let port;
+
+for (const arg in argv) {
+  switch (arg) {
+    case 'h':
+      usage();
+      break;
+    case 'p':
+      port = argv[arg];
+      break
+    case '_':
+      break;
+    default:
+      usage();
+      break;
+  }
+}
+
+const lint = async () => {
+  cd('../../')
+
+  await $`ls`;
+  if (typeof port === 'string') {
+    await $`pnpm lint \\
+      --filter="ledger-live-desktop" \\
+      --api="http://127.0.0.1:${port}" \\
+      --token="yolo" \\
+      --team="foo" \\
+      -- --format="json" \\
+      -o="lint.json"`;
+  } else {
+    await $`pnpm lint \\
+      --filter="ledger-live-desktop" \\
+      --token="yolo" \\
+      --team="foo" \\
+      -- --format="json" \\
+      -o="lint.json"`;
+  }
+
+  const lintJson = require('../lint.json')
+  stylish(lintJson);
+}
+
+
+await lint()
